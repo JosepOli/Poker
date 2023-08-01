b52dd1d0120e8dd59edf1347765db0e0983b296c
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-08-01 11:47:26 +0200

    First Commit

---
+++ poker/src/main/java/poker/poker/domain/Card.java
@@ -0,0 +1,19 @@
+package poker.poker.domain;
+
+public class Card {
+	
+	private int suit;
+	
+	private int number;
+	
+	final int CLUBS = 1;
+	final int DIAMONDS = 2;
+	final int HEARTS = 3;
+	final int SPADES = 4;
+	
+	public Card(int suit, int number) {
+		
+	}
+	
+
+}
---
+++ poker/src/main/java/poker/poker/domain/Player.java
@@ -0,0 +1,5 @@
+package poker.poker.domain;
+
+public class Player {
+
+}
