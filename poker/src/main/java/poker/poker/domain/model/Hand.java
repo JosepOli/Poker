daa0fbed4003fd69c992b4ff3183efac6c4f342a
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-08-01 12:57:45 +0200

    upd

--- poker/src/main/java/poker/poker/domain/model/Hand.java
+++ poker/src/main/java/poker/poker/domain/model/Hand.java
@@ -1,5 +1,36 @@
 package poker.poker.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hand {
+
+	int score;
+
+	private List<Card> hand;
+
+	public Hand() {
+		hand = new ArrayList<>();
+	}
+
+	public void addCard(Card card) {
+		hand.add(card);
+	}
+
+	public void removeCard(Card card) {
+		hand.remove(card);
+	}
+
+	public List<Card> getCards() {
+		return hand;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getScore() {
+		return score;
+	}
 
 }
