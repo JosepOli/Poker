eec94a097447f9b55fb4bd92b935095bed19039a
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-08-01 16:31:08 +0200

    Player class created

--- poker/src/main/java/poker/poker/domain/model/Player.java
+++ poker/src/main/java/poker/poker/domain/model/Player.java
@@ -2,4 +2,23 @@ package poker.poker.domain.model;
 
 public class Player {
 
+	private String name;
+	private int balance;
+
+	private Hand hand;
+
+	public Player(String name, int balance) {
+		this.name = name;
+		this.balance = balance;
+		this.hand = new Hand();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
 }
