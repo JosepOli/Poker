463fd97e91c704b18da1621bfc0d1fde2f7a5362
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-08-01 11:56:07 +0200

    Validation on constructor

--- poker/src/main/java/poker/poker/domain/model/Card.java
+++ poker/src/main/java/poker/poker/domain/model/Card.java
@@ -1,21 +1,26 @@
 package poker.poker.domain.model;
 
 public class Card {
-	
+
 	private int suit;
-	
+
 	private int number;
-	
+
 	final int CLUBS = 1;
 	final int DIAMONDS = 2;
 	final int HEARTS = 3;
 	final int SPADES = 4;
-	
+
 	public Card(int suit, int number) {
-		if(suit < CLUBS || suit > SPADES) {
-			
+		if (suit < CLUBS || suit > SPADES) {
+			throw new IllegalArgumentException("Invalid suit value");
 		}
+		if (number < 2 || number > 14) {
+			throw new IllegalArgumentException("Number must be between 2 and 14");
+		}
+
+		this.suit = suit;
+		this.number = number;
 	}
-	
 
 }
