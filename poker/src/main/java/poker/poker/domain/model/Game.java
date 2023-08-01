e873236dbb6b39f1ffa1ad3617c34c2961c66ff7
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-08-01 16:45:09 +0200

    Game atributes added

--- poker/src/main/java/poker/poker/domain/model/Game.java
+++ poker/src/main/java/poker/poker/domain/model/Game.java
@@ -1,5 +1,11 @@
 package poker.poker.domain.model;
 
+import java.util.List;
+
 public class Game {
+	private List<Player> players;
+	private Deck deck;
+	private int currentBet;
+	private int gameState;
 
 }
