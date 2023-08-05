7b012b2cf0a8483d9e2ddcb06b5bd3bfd8911fb8
JosepOli <josepolive89@gmail.com>
2023-08-05 22:54:44 +0200

    Method to determine the winner

--- poker/src/main/java/poker/poker/domain/service/GameService.java
+++ poker/src/main/java/poker/poker/domain/service/GameService.java
@@ -1,6 +1,29 @@
 package poker.poker.domain.service;
 
+import java.util.List;
+
+import poker.poker.domain.model.Game;
+import poker.poker.domain.model.Player;
+
 public class GameService {
 
-	
+	HandService handService = new HandService();
+
+	// Method to determine the winner
+	public Player determineWinner(Game game) {
+		int score;
+		int highestScore = 0;
+		Player winner = null;
+		List<Player> players = game.getPlayers();
+		for (Player player : players) {
+			score = handService.evaluateHand(player.getHand());
+			if (score > highestScore) {
+				highestScore = score;
+				winner = player;
+			}
+		}
+
+		return winner;
+	}
+
 }
