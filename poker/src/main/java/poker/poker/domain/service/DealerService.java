f44230253bd5551800423013fff2b1e4c9f39c86
JosepOli <josepolive89@gmail.com>
2023-08-05 20:27:39 +0200

    Added method to deal 5 cards to players

--- poker/src/main/java/poker/poker/domain/service/DealerService.java
+++ poker/src/main/java/poker/poker/domain/service/DealerService.java
@@ -1,10 +1,12 @@
 package poker.poker.domain.service;
 
 import java.util.Collections;
+import java.util.List;
 
 import poker.poker.domain.model.Card;
 import poker.poker.domain.model.Deck;
 import poker.poker.domain.model.Hand;
+import poker.poker.domain.model.Player;
 import poker.poker.domain.model.factory.DeckFactory;
 
 public class DealerService {
@@ -27,5 +29,13 @@ public class DealerService {
 	public void dealCardToHand(Hand hand) {
 		Card drawnCard = drawCard();
 		hand.addCard(drawnCard);
+	}
+
+	public void dealCardsToPlayers(List<Player> players) {
+		for (Player player : players) {
+			for (int i = 0; i < 5; i++) {
+				dealCardToHand(player.getHand());
+			}
+		}
 	}
 }
