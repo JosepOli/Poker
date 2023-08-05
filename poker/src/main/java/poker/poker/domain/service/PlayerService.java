36b84903df6a3b00c64c985709b75f816b5229f7
JosepOli <josepolive89@gmail.com>
2023-08-05 22:40:09 +0200

    PlayerService class modified to create names for IAplayers

--- poker/src/main/java/poker/poker/domain/service/PlayerService.java
+++ poker/src/main/java/poker/poker/domain/service/PlayerService.java
@@ -2,8 +2,13 @@ package poker.poker.domain.service;
 
 import poker.poker.domain.model.Hand;
 import poker.poker.domain.model.Player;
+import java.util.Random;
 
 public class PlayerService {
+
+	private String[] names = { "Albert Wilson", "Julio El Tutor", "Samuel Lightbringer", "Amador Heliocentrista",
+			"Kevin El Espanol" };
+	private Random random = new Random();
 
 	public Player createPlayer(String name, int balance) {
 		return new Player(name, balance);
@@ -11,6 +16,22 @@ public class PlayerService {
 
 	public void assignHand(Player player, Hand hand) {
 		player.setHand(hand);
+	}
+
+	public Player createIaPlayer() {
+		String name = generateRandomName();
+		int balance = generateRandomBalance();
+		return new Player(name, balance);
+	}
+
+	private String generateRandomName() {
+		int randomIndex = random.nextInt(names.length);
+		return names[randomIndex];
+	}
+
+	private int generateRandomBalance() {
+		// Generates a random balance between 100 and 5000
+		return random.nextInt(5000 - 100 + 1) + 100;
 	}
 
 }
