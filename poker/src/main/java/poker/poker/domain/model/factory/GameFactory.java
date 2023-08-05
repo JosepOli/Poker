771d239fa5f65115eb88bc4634cab1db3dde0fc6
JosepOli <josepolive89@gmail.com>
2023-08-05 22:41:03 +0200

    GameFactory class updated but not finished

--- poker/src/main/java/poker/poker/domain/model/factory/GameFactory.java
+++ poker/src/main/java/poker/poker/domain/model/factory/GameFactory.java
@@ -1,16 +1,34 @@
 package poker.poker.domain.model.factory;
 
-import poker.poker.domain.model.Deck;
 import poker.poker.domain.model.Game;
+import poker.poker.domain.model.Player;
 import poker.poker.domain.service.DealerService;
+import poker.poker.domain.service.PlayerService;
 
 public class GameFactory {
 
-	public Game createStandardGame(DeckFactory deckfactory) {
+	public Game createStandardGame(DeckFactory deckfactory, PlayerService playerService, int numIaPlayers) {
 		DealerService dealerService = new DealerService();
 		dealerService.shuffleDeck();
 		Game game = new Game(dealerService);
-		game.addPlayer(player);
+
+		addPlayersToGame(game, playerService, numIaPlayers);
+
+		// Deal cards to all players
+		dealerService.dealCardsToPlayers(game.getPlayers());
+
 		return game;
+	}
+
+	private void addPlayersToGame(Game game, PlayerService playerService, int numIaPlayers) {
+		// Create and add the human player
+		Player humanPlayer = playerService.createPlayer("Player", 5000);
+		game.addPlayer(humanPlayer);
+
+		// Create and add the IA players
+		for (int i = 0; i < numIaPlayers; i++) {
+			Player iaPlayer = playerService.createIaPlayer();
+			game.addPlayer(iaPlayer);
+		}
 	}
 }
