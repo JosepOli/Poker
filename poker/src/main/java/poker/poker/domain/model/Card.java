6384e301c6b0a52eb7e0d849361f412c3d77466c
JosepOli <josepolive89@gmail.com>
2023-08-07 13:09:02 +0200

    Added methods to get card number and suit visually iso in number form

--- poker/src/main/java/poker/poker/domain/model/Card.java
+++ poker/src/main/java/poker/poker/domain/model/Card.java
@@ -11,7 +11,8 @@ public class Card {
 
 	private int number;
 
-	//Constructor including validation that throws an IllegalArgumentException if values are not on range
+	// Constructor including validation that throws an IllegalArgumentException if
+	// values are not on range
 	public Card(int suit, int number) {
 		if (suit < CLUBS || suit > SPADES) {
 			throw new IllegalArgumentException("Invalid suit value");
@@ -30,6 +31,54 @@ public class Card {
 
 	public int getNumber() {
 		return number;
+	}
+
+	public String getSuitAsString() {
+		switch (this.suit) {
+		case CLUBS:
+			return "CLUBS";
+		case DIAMONDS:
+			return "DIAMONDS";
+		case HEARTS:
+			return "HEARTS";
+		case SPADES:
+			return "SPADES";
+		default:
+			return "UNKNOWN";
+		}
+	}
+
+	public String getNumberAsString() {
+		switch (this.number) {
+		case 2:
+			return "2";
+		case 3:
+			return "3";
+		case 4:
+			return "4";
+		case 5:
+			return "5";
+		case 6:
+			return "6";
+		case 7:
+			return "7";
+		case 8:
+			return "8";
+		case 9:
+			return "9";
+		case 10:
+			return "10";
+		case 11:
+			return "Jack";
+		case 12:
+			return "Queen";
+		case 13:
+			return "King";
+		case 14:
+			return "Ace";
+		default:
+			return "Unknown";
+		}
 	}
 
 }
