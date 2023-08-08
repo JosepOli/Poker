c86348d6404a824b54999edca976ba718ecd517e
JosepOli <josepolive89@gmail.com>
2023-08-08 12:47:21 +0200

    upd
    Updated GameService to handle ties,

--- poker/src/main/java/poker/poker/domain/service/GameService.java
+++ poker/src/main/java/poker/poker/domain/service/GameService.java
@@ -1,8 +1,10 @@
 package poker.poker.domain.service;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import poker.poker.domain.model.Game;
+import poker.poker.domain.model.Hand;
 import poker.poker.domain.model.Player;
 
 public class GameService {
@@ -11,16 +13,31 @@ public class GameService {
 
 	// Method to determine the winner
 	public Player determineWinner(Game game) {
-		int score;
 		int highestScore = 0;
 		Player winner = null;
+		int tieCount = 0;
 		List<Player> players = game.getPlayers();
+		List<Player> playersTied = new ArrayList<>();
 		for (Player player : players) {
-			score = handService.evaluateHand(player.getHand());
+
+			Hand hand = handService.evaluateHand(player.getHand());
+			int score = hand.getScore();
 			if (score > highestScore) {
 				highestScore = score;
 				winner = player;
+				tieCount = 1;
+				playersTied.clear(); // Clear the list of tied players
+				playersTied.add(player); // Add the current player as they have the highest score for now
+			} else if (score == highestScore) {
+				tieCount++;
+				playersTied.add(player);
 			}
+
+		}
+
+		if (tieCount > 1) {
+			winner = handService.tieBreak(playersTied);
+
 		}
 
 		return winner;
